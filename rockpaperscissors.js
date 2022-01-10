3d57f26fb67c20be9c46783baaf053ba62e7a591
JP Moregain <j.p.moregain15@gmail.com>
2022-01-10 10:24:59 -0600

    Update

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -4,11 +4,43 @@ function computerPlay(options) {
     return options[Math.floor(Math.random()*options.length)];
 }
 //determine winner
-  const playerSelection = window.prompt('Please choose from the following: rock, paper, scissors');
+  const userInput = window.prompt('Please choose from the following: rock, paper, scissors');
   const computerSelection = computerPlay(options);
+  const playerSelection = userInput.toLowerCase();
   function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase();
-    return "You Lose! " + playerSelection + " beats " + computerSelection;
+      if (playerSelection === computerSelection) {
+        return "It's a tie!";
+      }
+        else if (playerSelection == "rock") {
+            if (computerSelection == "paper") {
+                return "You Lose! Paper beats Rock";
+          }
+        }
+        else if (playerSelection == "paper") {
+            if (computerSelection == "scissors") {
+                return "You Lose! Scissors beats Paper"   
+            }  
+        }
+        else if (playerSelection == "scissors") {
+            if (computerSelection == "rock") {
+                return "You Lose! Rock beats Scissors" 
+            }
+        }
+        else if (playerSelection == "paper") {
+            if (computerSelection == "rock") {
+                return "You Win! Paper beats Rock"
+            }
+        }
+        else if (playerSelection == "scissors") {
+            if (computerSelection == "paper") {
+                return "You Win! Scissors beats Paper"
+            }
+        }
+        else if (playerSelection == "rock") {
+            if (computerSelection == "scissors") {
+                return "You Win! Rock beats Scissors"
+            }
+        }      
   }
   console.log(playRound(playerSelection, computerSelection));
 //declare winner
